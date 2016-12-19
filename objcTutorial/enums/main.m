aca7d06bb24f9ec0bb75f6d1207fd60d2798229e
Jeff, juain farn, liu <jeffliu@gisbim.com>
2016-12-19 10:05:05 +0800

    add exercise for enums

---
+++ objcTutorial/enums/main.m
@@ -0,0 +1,30 @@
+// main.m
+// exercise for enums
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+	FORD,
+	HONDA,
+	NISSAN,
+	PORSCHE
+} CarModel;
+
+int main(int argc, const char *argv[]) {
+    @autoreleasepool {
+        CarModel myCar = PORSCHE;
+        switch(myCar) {
+        	case FORD:
+        	case PORSCHE:
+        	     NSLog(@"You like Western cars ?");
+        	     break;
+        	case HONDA:
+        	case NISSAN:
+        	     NSLog(@"You like Japanese cars ?");
+        	     break;
+        	default:
+        	     break;
+        }
+    }
+    return 0;
+}
